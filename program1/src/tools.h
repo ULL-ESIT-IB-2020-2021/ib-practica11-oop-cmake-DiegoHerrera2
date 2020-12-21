dba86990013046b82860f20ae39ee700b6d89b1c
DiegoHerrera2 <alu0101408903@ull.edu.es>
2020-12-21 22:05:55 +0100

    Primer programa

---
+++ program1/src/tools.h
@@ -0,0 +1,18 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author Diego Herrera Mendoza
+ * @date 21.dec.2020
+ * @brief En este archivo se declaran las funciones del programa
+ *
+ */
+
+#include <iostream>
+
+const std::string kHelpText = "Este programa representa y gestiona fechas que el usuario \
+ha de introducir por línea de comandos para la ejecución del programa";
+
+void Usage(int argc, char *argv[]);
